137e38fe872a41f01c9f9382f080986dfae36b59
Uvan Sankar <uvansankar1999@gmail.com>
2022-07-10 11:39:11 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Register from "./components/register/Register"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import "./App.css "
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col justify-center items-center">
       <Router>
         <Routes>
           <Route path="/" element={<Register />}></Route>
